82213a735f834d36181af2e45fb5c1feaf9995be
Feilong Ma <mafeilong@gmail.com>
2013-07-11 15:51:32 +0800

    Updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var buf = fs.readFileSync('index.html');
+  var msg = buf.toString();
+  response.send(msg);
 });
 
 var port = process.env.PORT || 5000;
